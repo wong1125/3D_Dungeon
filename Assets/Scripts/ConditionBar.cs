6455adebd265a6a18c2a078557bd7c6b09863855
wong1125 <wong1125@naver.com>
2025-08-08 17:55:21 +0900

    [Feat] 체력바 UI 생성

---
+++ Assets/Scripts/ConditionBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConditionBar : MonoBehaviour
+{
+    [SerializeField] Image uiBar;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
