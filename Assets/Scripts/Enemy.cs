ff988616b90b49dce2a1b9fb1ad0f219daa99141
wong1125 <wong1125@naver.com>
2025-08-13 11:47:09 +0900

    [Feat] 적 AI 구현

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,25 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-public enum AIState 
-{ 
-    Idle,
-    Following
-}
 
 
 public class Enemy : MonoBehaviour
 {
-    private AIState aistate;
 
     [SerializeField] float playerFollowDistance;
     private float distancePlayer;
-    
+    private NavMeshAgent agent;
+    private Vector3 startPosition;
+    private float lastCheckedTime;
+    private float resetLimt = 30f;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        aistate = AIState.Idle;
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -28,9 +32,27 @@ public class Enemy : MonoBehaviour
         distancePlayer = (transform.position - CharacterManager.Instance.Player.transform.position).sqrMagnitude;
         if (distancePlayer < playerFollowDistance)
         {
-            aistate = AIState.Following;
+            FollowPlayer();
+            lastCheckedTime = Time.time;
         }
-        else 
-            aistate = AIState.Idle;
+        else if (Time.time - lastCheckedTime > resetLimt)
+            transform.position = startPosition;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            CharacterManager.Instance.Player.condition.HealthChanger(-10);
+        }
+    }
+
+    void FollowPlayer()
+    {
+        NavMeshPath path = new NavMeshPath();
+        if (agent.CalculatePath(CharacterManager.Instance.Player.transform.position, path))
+        {
+            agent.SetDestination(CharacterManager.Instance.Player.transform.position);
+        }
     }
 }
